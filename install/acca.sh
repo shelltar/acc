0b59fd2e9b56a124ef810ab855ad03717d39da63
VR-25 <28943176+VR-25@users.noreply.github.com>
2024-04-07 12:12:43 +0100

    Speed up acca --set for voltage and current limits

--- install/acca.sh
+++ install/acca.sh
@@ -94,15 +94,15 @@ case "$@" in
 
     export "$@"
 
-    [ .${mcc-${max_charging_current-x}} = .x ] || {
-      . $execDir/set-ch-curr.sh
-      set_ch_curr ${mcc:-${max_charging_current:--}} || :
-    }
+    # [ .${mcc-${max_charging_current-x}} = .x ] || {
+    #   . $execDir/set-ch-curr.sh
+    #   set_ch_curr ${mcc:-${max_charging_current:--}} || :
+    # }
 
-    [ ".${mcv-${max_charging_voltage-x}}" = .x ] || {
-      . $execDir/set-ch-volt.sh
-      set_ch_volt "${mcv:-${max_charging_voltage:--}}" || :
-    }
+    # [ ".${mcv-${max_charging_voltage-x}}" = .x ] || {
+    #   . $execDir/set-ch-volt.sh
+    #   set_ch_volt "${mcv:-${max_charging_voltage:--}}" || :
+    # }
 
     . $execDir/write-config.sh
     exit 0
